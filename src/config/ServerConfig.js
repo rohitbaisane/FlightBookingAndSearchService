8afa6731de415ab6127d9dac17eecdddf3185524
Rohit baisane <rohitbaisane91@gmail.com>
2022-12-18 13:47:42 +0530

    Setup basic dotenv

--- src/config/ServerConfig.js
+++ src/config/ServerConfig.js
@@ -1,1 +1,10 @@
 //Server configuartion setup
+
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+
+module.exports = {
+    PORT: process.env.PORT
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 const express = require('express');
-
+const { PORT } = require('./config/serverConfig');
 const setupAndStartServer = async() => {
 
     //create the express object and make it listen to PORT.
     const app = express();
-    const PORT = 3000;
     app.listen(PORT, () => {
         console.log(`Server started at ${PORT}`);
     });
